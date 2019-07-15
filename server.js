ea91828457949cc3b5ed8d8064e8cc353393437a
omgitsgod <omgitsgod@gmail.com>
2019-07-14 23:48:38 -0400

    Comment out last commit

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.get(
 	function(req, res) {
     const user = req.user;
 		const token = user.token;
-    req.session.user = req.user;
-    console.log('session: ', req.session);
+    //req.session.user = req.user;
+    //console.log('session: ', req.session);
     logged.push(user)
     loggedIn.includes(user) ? null : loggedIn.push(user)
     console.log(loggedIn);
