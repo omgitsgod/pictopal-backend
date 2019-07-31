30674275eee62bbd77bd6aa3571452b3d65785c2
omgitsgod <omgitsgod@gmail.com>
2019-07-31 18:59:02 -0400

    Make useCreateIndex: true

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const User = require('./user')
 
 const connectDb = () => {
-  return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pictopal', { useNewUrlParser: true });
+  return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pictopal', { useNewUrlParser: true, useCreateIndex: true, });
 };
 
 const models = {User};
