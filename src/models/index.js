d7c3ffabd3647321e3e129d3be2a13dce4137354
omgitsgod <omgitsgod@gmail.com>
2019-07-31 18:56:46 -0400

    Make useNewUrlParser: true

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const User = require('./user')
 
 const connectDb = () => {
-  return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pictopal');
+  return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pictopal', { useNewUrlParser: true });
 };
 
 const models = {User};
