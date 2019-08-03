ec7992756622b1490a7c244e1f610de25dbe36b2
omgitsgod <omgitsgod@gmail.com>
2019-08-03 17:10:15 -0400

    Require User in passport

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const User = require('./user')
 
 passport.serializeUser((user, done) => {
   console.log("Serializer : ", user)
