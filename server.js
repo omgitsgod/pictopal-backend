bc793a052c1bda8ce990a6f001a4468ca142ded9
omgitsgod <omgitsgod@gmail.com>
2019-08-04 17:17:25 -0400

    point ws to sockets file

--- server.js
+++ server.js
@@ -65,19 +65,6 @@ connectDb()
 
 app.use('/', routes);
 
-app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
-    ws.send(`You just said: ${msg}`)
-  });
-  ++clients
-  ws.send('Hello! Message From Server!!')
-  console.log('clients:', clients);
-
-  ws.on('close', () => {
-    --clients
-    console.log(`user disconnected, Clients: ${clients}`);
-});
-});
+app.ws('/', sockets);
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
