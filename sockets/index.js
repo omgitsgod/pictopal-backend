aebe006d57bebe256ebb0913bcaedf781906039e
omgitsgod <omgitsgod@gmail.com>
2019-08-11 23:52:55 -0400

    Add user to livelist when on socket

--- sockets/index.js
+++ sockets/index.js
@@ -1,15 +1,16 @@
 const {models} = require('../src/models')
 let {clients, liveList} = require('../constants')
+let user;
 
 sockets = (ws, req) => {
   if (req.session && req.session.passport) {
-    let user;
     models.User.findById(req.session.passport.user, (err, u) => {
       if (err) {
         console.log(err);
       } else {
         user = u
         console.log('YOU ARE ON THE SOCKET AS:', user.name);
+        liveList.push(user)
       }
     })
   }
@@ -25,6 +26,7 @@ sockets = (ws, req) => {
   ws.on('close', () => {
     --clients
     console.log(`user disconnected, Clients: ${clients}`);
+    liveList = liveList.filter(x => x.token !== req.session.user.token)
 });
 }
 module.exports = sockets
