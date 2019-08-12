bda53265956dab8fefd779536df3baedef4ca7a3
omgitsgod <omgitsgod@gmail.com>
2019-08-11 23:36:10 -0400

    Get user from the database on socket connection

--- sockets/index.js
+++ sockets/index.js
@@ -2,7 +2,17 @@ const {models} = require('../src/models')
 let {clients, liveList} = require('../constants')
 
 sockets = (ws, req) => {
-  console.log("YOU ARE CONNECTED");
+  if (req.session && req.session.passport) {
+    let user
+    models.User.findById(req.session.passport.user, (err, u) => {
+      if (err) {
+        console.log(err);
+      } else {
+        user = u
+      }
+    }
+    console.log("you are on the socket as", user);
+  }
   ws.on('message', function(msg) {
     console.log(msg);
     console.log(req.session);
