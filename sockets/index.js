342ccd768b8395219bfe7b25dbc05b3483cd59bd
omgitsgod <omgitsgod@gmail.com>
2019-08-14 21:02:36 -0400

    Move order of ++client

--- sockets/index.js
+++ sockets/index.js
@@ -14,13 +14,13 @@ sockets = (ws, req) => {
       }
     })
   }
+  ++clients
+  ws.send('Hello! Message From Server!!')
   ws.on('message', function(msg) {
     console.log(msg);
     console.log(req.session);
     ws.send(`You just said: ${msg}`)
   });
-  ++clients
-  ws.send('Hello! Message From Server!!')
   console.log('clients:', clients);
 
   ws.on('close', () => {
