77b7e651e52f2c890f0f0cd6a95a4a50a278f06c
omgitsgod <omgitsgod@gmail.com>
2019-08-20 15:07:02 -0400

    Move opening socket logic to on open

--- sockets/index.js
+++ sockets/index.js
@@ -3,31 +3,33 @@ let {clients, liveList} = require('../constants');
 let user;
 
 sockets = (ws, req) => {
-  if (req.session && req.session.passport) {
-    models.User.findById(req.session.passport.user, (err, u) => {
-      if (err) {
-        console.log(err);
-      } else {
-        user = u;
-        console.log('YOU ARE ON THE SOCKET AS:', user.name);
-        liveList.push(user);
-      }
-    })
-  }
-  if (clients === 0) {
-    req.session.ws = 'host';
-  } else {
-    req.session.ws = 'client';
-  }
-  ++clients;
-  ws.on('message', function(msg) {
+  ws.on('open', () => {
+    if (req.session && req.session.passport) {
+      models.User.findById(req.session.passport.user, (err, u) => {
+        if (err) {
+          console.log(err);
+        } else {
+          user = u;
+          console.log('YOU ARE ON THE SOCKET AS:', user.name);
+          liveList.push(user);
+        }
+      })
+    }
+    if (clients === 0) {
+      req.session.ws = 'host';
+    } else {
+      req.session.ws = 'client';
+    }
+    ++clients;
+    console.log('clients:', clients);
+  });
+  ws.on('message', (msg) => {
     console.log(req.session);
     if (req.session.ws === 'host') {
       console.log(msg);
       ws.send(msg);
     }
   });
-  console.log('clients:', clients);
 
   ws.on('close', () => {
     --clients;
