70ddfc5ce71df986e12821bcc76bbf51962fb066
omgitsgod <omgitsgod@gmail.com>
2019-08-30 17:08:42 -0400

    Comment out objs

--- sockets/index.js
+++ sockets/index.js
@@ -28,8 +28,8 @@ sockets = (ws, req) => {
     if (req.session.ws === 'host') {
       console.log(msg);
       ws.send(msg);
-      const objs = JSON.parse(msg)
-      console.log(objs);
+    //  const objs = JSON.parse(msg).objects
+    //  console.log(objs);
     }
   });
 
