02cebfd13c7d051a183e7eb43416cab492d85079
omgitsgod <omgitsgod@gmail.com>
2019-08-30 21:36:52 -0400

    Change conditional to client

--- sockets/index.js
+++ sockets/index.js
@@ -24,7 +24,7 @@ sockets = (ws, req) => {
 
   ws.on('message', (msg) => {
     console.log(req.session);
-    if (req.session.ws === 'host') {
+    if (req.session.ws === 'client') {
       ws.send(msg);
       console.log("incoming: ", msg);
       console.log("msg type: ", typeof msg)
